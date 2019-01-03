c058a7c53eddbcef8484e25801b4c781ac1226f3
stevemacenski <stevenmacenski@gmail.com>
2019-01-03 09:54:13 -0800

    adding support for master hostnames in launching

--- rs_camera_snap.sh
+++ rs_camera_snap.sh
@@ -5,12 +5,15 @@ serial_no=`snapctl get serialno`
 camera=`snapctl get cameraid`
 profile=`snapctl get profile`
 masterhostname=`snapctl get masterhostname`
+if [[ $masterhostname == "" ]]; then
+  masterhostname='localhost'
+fi
 
 # set global ones to empty again to reset
 sudo snap set ros-realsense-d400 serialno="" cameraid="" profile="" masterhostname=""
 
 if [[ $profile = 1 ]]; then
-  roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 enable_infra1:=false enable_infra2:=false 
+  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 enable_infra1:=false enable_infra2:=false 
 else
-  roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera
+  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera
 fi
