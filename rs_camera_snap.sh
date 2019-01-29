a8d7c5cbfbd25721d4c0e14d81def6a6c801af87
stevemacenski <stevenmacenski@gmail.com>
2019-01-28 16:10:16 -0800

    adding infra1 + infra2 fps 6

--- rs_camera_snap.sh
+++ rs_camera_snap.sh
@@ -17,7 +17,7 @@ fi
 sudo snap set ros-realsense-d400 serialno="" cameraid="" profile="" masterhostname="" irone=""
 
 if [[ $profile = 1 ]]; then
-  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 enable_infra1:=${irone} enable_infra2:=false
+  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 enable_infra1:=${irone} enable_infra2:=false
 else
   ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera
 fi
