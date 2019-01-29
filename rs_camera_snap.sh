cb1a9ebdf8d52277f0b2885f612f81585509fa6d
stevemacenski <stevenmacenski@gmail.com>
2019-01-28 17:27:09 -0800

    424x240

--- rs_camera_snap.sh
+++ rs_camera_snap.sh
@@ -17,7 +17,7 @@ fi
 sudo snap set ros-realsense-d400 serialno="" cameraid="" profile="" masterhostname="" iron=""
 
 if [[ $profile = 1 ]]; then
-  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 enable_infra1:=${iron} enable_infra2:=${iron}
+  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 infra1_width:=424 infra1_height:=240 infra2_width:=424 infra2_height:=240 enable_infra1:=${iron} enable_infra2:=${iron}
 else
   ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera
 fi
