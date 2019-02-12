80d0043fdef0d47fb7775a90c8b6753e80830553
stevemacenski <stevenmacenski@gmail.com>
2019-02-11 16:59:46 -0800

    adding profile 3 for full depth and downsampled color

--- rs_camera_snap.sh
+++ rs_camera_snap.sh
@@ -20,6 +20,8 @@ if [[ $profile = 1 ]]; then
   ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 color_width:=320 color_height:=180 depth_width:=424 depth_height:=240 infra1_width:=424 infra1_height:=240 infra2_width:=424 infra2_height:=240 enable_infra1:=${iron} enable_infra2:=${iron}
 elif [[ $profile = 2 ]]; then
   ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 enable_infra1:=${iron} enable_infra2:=${iron}
+elif [[ $profile = 3 ]]; then
+  ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera depth_fps:=6 color_fps:=6 infra1_fps:=6 infra2_fps:=6 color_width:=320 color_height:=180 enable_infra1:=${iron} enable_infra2:=${iron}
 else
   ROS_MASTER_URI=http://${masterhostname}:11311 roslaunch --wait realsense2_camera rs_camera.launch serial_no:=$serial_no camera:=$camera
 fi
